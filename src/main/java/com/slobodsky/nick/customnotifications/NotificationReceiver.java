fb6edd45259fc7ccecb32acb930e206d04abcff8
nicksl1986 <34469459+nicksl1986@users.noreply.github.com>
2019-02-22 00:28:30 +0200

    Add files via upload

---
+++ src/main/java/com/slobodsky/nick/customnotifications/NotificationReceiver.java
@@ -0,0 +1,21 @@
+package com.slobodsky.nick.customnotifications;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationManagerCompat;
+import android.widget.Toast;
+
+public class NotificationReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Toast.makeText(context, "Image clicked", Toast.LENGTH_SHORT).show();
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+        notificationManager.cancel(1);
+
+    }
+}
